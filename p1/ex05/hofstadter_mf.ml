4248d6101cbf8800bae4c48bbf043e236988e7e6
aldisti <alessandrodiste99@gmail.com>
2025-08-14 21:30:06 +0200

    now using mutual recursion instead of "garbage"

--- p1/ex05/hofstadter_mf.ml
+++ p1/ex05/hofstadter_mf.ml
@@ -1,15 +1,13 @@
 
-let rec hfs_f ?(def=1) n : int =
-  let def_m = if def == 1 then 0 else 1 in
+let rec hfs_f n : int =
   if n < 0 then -1
-  else if n <= 1 then def
-  else (n - (hfs_f ~def:def_m (hfs_f ~def:def (n - 1))))
+  else if n == 0 then 1
+  else (n - (hfs_m (hfs_f (n - 1))))
 
-let rec hfs_m ?(def=0) n : int =
-  let def_f = if def == 0 then 1 else 0 in
+and hfs_m n : int =
   if n < 0 then -1
-  else if n <= 1 then def
-  else (n - (hfs_m ~def:def_f (hfs_m (n - 1))))
+  else if n == 0 then 0
+  else (n - (hfs_f (hfs_m (n - 1))))
 
 
 (*
@@ -35,7 +33,6 @@ M(4) = 2
 (* TEST SUITE *)
 
 let () = (
-
   let test n f_res m_res =
     let print n actual res = 
       Printf.printf "%s - %d = %d\n" (if actual = res then "OK" else "KO") n actual
