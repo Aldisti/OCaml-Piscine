9554d6e356c02a8d06c413c35049552978945108
aldisti <alessandrodiste99@gmail.com>
2025-08-14 10:27:05 +0200

    ex00 done

---
+++ p0/ex00/ft_test_sign.ml
@@ -0,0 +1,11 @@
+
+let ft_test_sign (n: int) : unit =
+    if n >= 0 then
+        print_endline "positive"
+    else
+        print_endline "negative"
+    ;;
+
+ft_test_sign 42;;
+ft_test_sign 0;;
+ft_test_sign (-42);;
