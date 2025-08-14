b298e5602db62ade0890b198420d74605f0027b4
aldisti <alessandrodiste99@gmail.com>
2025-08-14 11:38:33 +0200

    ex06 done

---
+++ p0/ex06/ft_string_all.ml
@@ -0,0 +1,26 @@
+
+let ft_string_all (p: char -> bool) (s: string) : bool =
+  let i = ref 0 in
+    while !i < String.length s && p (String.get s !i) do (
+      i := (!i + 1)
+    ) done;
+  !i = String.length s
+;;
+
+(* TEST SUITE *)
+
+let test (p: char -> bool) s : unit =
+  if ft_string_all p s then
+    print_endline "true" else print_endline "false"
+;;
+
+let is_digit = fun c -> c >= '0' && c <= '9';;
+let is_lower = fun c -> (c >= 'a' && c <= 'z');;
+
+test is_digit "1234";;
+test is_digit "12A4";;
+test is_digit "";;
+
+test is_lower "ciao";;
+test is_lower "COME";;
+test is_lower "stAi";;
