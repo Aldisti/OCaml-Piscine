0d44681afb64542144ad4a18b4be1750710fcc9c
aldisti <alessandrodiste99@gmail.com>
2025-08-14 16:25:43 +0200

    day 1 ex02 done

---
+++ p1/ex02/ackermann.ml
@@ -0,0 +1,19 @@
+
+let rec ackermann (m: int) (n: int) : int =
+  if m < 0 || n < 0 then -1 else (
+    if m = 0 then n + 1
+    else if m > 0 && n == 0 then ackermann (m - 1) 1
+    else ackermann (m - 1) (ackermann m (n - 1))
+  )
+
+(* TEST SUITE *)
+
+let () = (
+  let test = fun m n ->
+    Printf.printf "%d %d => %d\n" m n (ackermann m n)
+  in
+  test (-1) 7;
+  test 0 0;
+  test 2 3;
+  test 4 1
+)
