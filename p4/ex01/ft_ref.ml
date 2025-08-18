eaaf21d16a9de35b94f962cef9abd24967134d8b
aldisti <alessandrodiste99@gmail.com>
2025-08-18 14:43:50 +0200

    day 4 ex01 done

---
+++ p4/ex01/ft_ref.ml
@@ -0,0 +1,31 @@
+
+type 'a ft_ref = {mutable contents: 'a}
+
+let return (v: 'a) : 'a ft_ref =
+  {contents = v}
+
+let get (r: 'a ft_ref) : 'a =
+  r.contents
+
+let set (r: 'a ft_ref) (v: 'a) : unit =
+  r.contents <- v
+
+let bind (r: 'a ft_ref) (f: 'a -> 'b ft_ref) : 'b ft_ref =
+  r.contents |> f
+
+
+let () =
+  let r1 = return 0 in
+  let r2 = r1 in
+
+  Printf.printf "r1: %d - r2: %d\n" (get r1) (get r2);
+
+  set r2 16;
+
+  Printf.printf "r1: %d - r2: %d\n" (get r1) (get r2);
+
+  let binder = fun x -> return @@ string_of_int @@ x * 3 in
+
+  let r3 = bind r1 binder in
+
+  Printf.printf "r1: %d - r3: %s\n" (get r1) (get r3);
