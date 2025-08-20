2ab2d8e0d5587baad1c80f1ea3eff23956994093
aldisti <alessandrodiste99@gmail.com>
2025-08-20 11:27:42 +0200

    day 6 ex01 done

---
+++ p6/ex01/main.ml
@@ -0,0 +1,26 @@
+
+let () =
+  let p = new People.people "Robin" in
+
+  print_endline "\nTest: initializer";
+  let d = new Doctor.doctor "Who" 42 p in
+
+  print_endline "\nTest: to_string";
+  Printf.printf "d = %s\n" d#to_string;
+
+  print_endline "\nTest: talk";
+  d#talk;
+
+  print_endline "\nTest: travel_in_time";
+  d#travel_in_time 2025 1950;
+  d#travel_in_time 2025 2035;
+  Printf.printf "d = %s\n" d#to_string;
+
+  print_endline "\nTest: take_damage";
+  d#take_damage 42;
+  Printf.printf "d = %s\n" d#to_string;
+
+  print_endline "\nTest: use_sonic_screwdriver";
+  d#use_sonic_screwdriver;
+  Printf.printf "d = %s\n" d#to_string;
+
