4c9d1e8ec2d58ef68a62ad1a9666696d8790d773
aldisti <alessandrodiste99@gmail.com>
2025-08-22 10:19:20 +0200

    d1 ex02 ready

--- p1/ex02/ackermann.ml
+++ p1/ex02/ackermann.ml
@@ -1,6 +1,7 @@
 
 let rec ackermann (m: int) (n: int) : int =
-  if m < 0 || n < 0 then -1 else (
+  if m < 0 || n < 0 then -1
+  else (
     if m = 0 then n + 1
     else if m > 0 && n == 0 then ackermann (m - 1) 1
     else ackermann (m - 1) (ackermann m (n - 1))
@@ -8,7 +9,7 @@ let rec ackermann (m: int) (n: int) : int =
 
 (* TEST SUITE *)
 
-let () = (
+let () =
   let test = fun m n ->
     Printf.printf "%d %d => %d\n" m n (ackermann m n)
   in
@@ -16,4 +17,3 @@ let () = (
   test 0 0;
   test 2 3;
   test 4 1
-)
