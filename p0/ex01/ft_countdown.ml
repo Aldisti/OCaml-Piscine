dd304490f0b3e0bd27aca88a7ec9dd7def5da0ea
aldisti <alessandrodiste99@gmail.com>
2025-08-22 09:21:16 +0200

    d0 ex01 ready

--- p0/ex01/ft_countdown.ml
+++ p0/ex01/ft_countdown.ml
@@ -1,6 +1,10 @@
 
 let rec ft_countdown (n: int) : unit =
-    if (n < 0) then () else (
+    if (n <= 0) then (
+        print_int 0;
+        print_char '\n';
+    )
+    else (
         print_int n;
         print_char '\n';
         ft_countdown (n - 1)
@@ -8,12 +12,10 @@ let rec ft_countdown (n: int) : unit =
 
 (* TEST SUITE *)
 
-let main unit =
-    print_string "-42:\n";
+let () =
+    print_string "----- -42 -----\n";
     ft_countdown (-42);
-    print_string "0:\n";
+    print_string "----- 0 -----\n";
     ft_countdown 0;
-    print_string "5:\n";
+    print_string "----- 5 -----\n";
     ft_countdown 5
-
-let () = main ()
