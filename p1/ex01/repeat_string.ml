e8c0164ecf4f19b6e95bac263f4aa99fef025601
aldisti <alessandrodiste99@gmail.com>
2025-08-22 10:14:19 +0200

    d1 ex01 ready

--- p1/ex01/repeat_string.ml
+++ p1/ex01/repeat_string.ml
@@ -1,13 +1,19 @@
 
-let rec repeat_string ?(str="x") (n: int) : string =
+let repeat_string ?(str="x") (n: int) : string =
+  let rec loop acc i =
+    if i <= 0 then acc
+    else loop (acc ^ str) (i - 1)
+  in
   if n < 0 then "Error"
-  else if n == 0 then ""
-  else str ^ (repeat_string ~str:str (n - 1))
+  else loop "" n
 
 (* TEST SUITE *)
 
-let () = (
-  print_endline (repeat_string 5);
-  print_endline (repeat_string ~str:"a" 3);
+let () =
   print_endline (repeat_string ~str:"a" (-1));
-)
+  print_endline (repeat_string 5);
+  print_endline (repeat_string ~str:"What" 3);
+  print_endline (repeat_string ~str:"foo" 1);
+  print_endline (repeat_string ~str:"" 3);
+  print_endline (repeat_string ~str:"Say it again!?" 0);
+
