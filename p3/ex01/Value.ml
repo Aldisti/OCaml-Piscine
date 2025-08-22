d5a27ad6d74856e1dde30ea631846dc238347384
aldisti <alessandrodiste99@gmail.com>
2025-08-22 11:17:17 +0200

    d3 ex01 ready

--- p3/ex01/Value.ml
+++ p3/ex01/Value.ml
@@ -1,4 +1,4 @@
-type value = T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10 | Jack | Queen | King | Ace
+type t = T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10 | Jack | Queen | King | Ace
 
 (** The list of all values of type t *)
 let all = [T2; T3; T4; T5; T6; T7; T8; T9; T10; Jack; Queen; King; Ace]
