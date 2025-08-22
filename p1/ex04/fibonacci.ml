e7b57227537adb89098b21750e76ad3d4b121f42
aldisti <alessandrodiste99@gmail.com>
2025-08-22 10:24:59 +0200

    d1 ex04 ready

--- p1/ex04/fibonacci.ml
+++ p1/ex04/fibonacci.ml
@@ -1,13 +1,16 @@
 
-let rec fibonacci ?(acc=0) ?(b=1) (n: int) : int =
+let rec fibonacci (n: int) : int =
+  let rec loop acc b n =
+    if n = 0 then acc
+    else if n = 1 then b
+    else loop b (acc + b) (n - 1)
+  in
   if n < 0 then -1
-  else if n == 0 then acc
-  else if n == 1 then b
-  else fibonacci ~acc:b ~b:(acc + b) (n - 1)
+  else loop 0 1 n
 
 (* TEST SUITE *)
 
-let () = (
+let () =
   let test n res =
     let actual = fibonacci n in
     Printf.printf "%s - %d => %d\n" (if actual = res then "OK" else "KO") n actual
@@ -19,4 +22,3 @@ let () = (
   test 3 2;
   test 9 34;
   test 15 610;
-)
