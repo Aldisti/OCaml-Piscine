0c1d0cd20c4c18e346861202c55e9dca2db36c87
aldisti <alessandrodiste99@gmail.com>
2025-08-22 09:40:43 +0200

    d0 ex05 ready

--- p0/ex05/ft_print_rev.ml
+++ p0/ex05/ft_print_rev.ml
@@ -1,12 +1,14 @@
 
 let ft_print_rev (s: string) : unit =
-  let rec loop = fun i j -> (
-    if i < j then () else (
+  let rec loop i j = (
+    if i < j then ()
+    else (
       print_char (String.get s i);
       loop (i - 1) j
     )
   ) in
-  loop (String.length s - 1) 0; print_char '\n'
+  loop (String.length s - 1) 0;
+  print_char '\n'
 
 (* TEST SUITE *)
 
@@ -15,9 +17,8 @@ let test s r : unit =
   print_string r;
   print_char '\n'
 
-let main unit =
+let () =
   test "ciao" "oaic";
-  test "" "";
-  test "0123456789" "9876543210"
-
-let () = main ()
+  test "0123456789" "9876543210";
+  test "Hello world !" "! dlrow olleH";
+  test "" ""
