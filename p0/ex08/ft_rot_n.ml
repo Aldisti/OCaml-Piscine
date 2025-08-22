8fadc584aa57e7539e8c50497b4cba090cba2012
aldisti <alessandrodiste99@gmail.com>
2025-08-22 10:02:49 +0200

    d0 ex08 ready

--- p0/ex08/ft_rot_n.ml
+++ p0/ex08/ft_rot_n.ml
@@ -1,31 +1,30 @@
 
 let ft_rot_n (n: int) (s: string) : string =
+  let map_char c n a =
+    char_of_int (((int_of_char c) - a + n) mod 26 + a)
+  in
   String.map (fun c -> (
     if c >= 'a' && c <= 'z' then
-      char_of_int (((int_of_char c) - 97 + n) mod 26 + 97)
+      map_char c n 97
     else if c >= 'A' && c <= 'Z' then
-      char_of_int (((int_of_char c) - 65 + n) mod 26 + 65)
+      map_char c n 65
     else c
   )) s
 
 (* TEST SUITE *)
 
-let test s1 s2 : unit =
+let test n s : unit =
   print_char '|';
-  print_string s1;
-  print_char '|';
-  print_string " == ";
-  print_char '|';
-  print_string s2;
+  print_string s;
+  print_string "| => |";
+  print_string (ft_rot_n n s);
   print_string "|\n"
 
-let main unit =
-  test (ft_rot_n 1 "abcdefghijklmnopqrstuvwxyz") "bcdefghijklmnopqrstuvwxyza";
-  test (ft_rot_n 13 "abcdefghijklmnopqrstuvwxyz") "nopqrstuvwxyzabcdefghijklm";
-  test (ft_rot_n 42 "0123456789") "0123456789";
-  test (ft_rot_n 2 "OI2EAS67B9") "QK2GCU67D9";
-  test (ft_rot_n 0 "Damned !") "Damned !";
-  test (ft_rot_n 42 "") "";
-  test (ft_rot_n 1 "NBzlk qnbjr !") "OCaml rocks !"
-
-let () = main ()
+let () =
+  test 1 "abcdefghijklmnopqrstuvwxyz";
+  test 13 "abcdefghijklmnopqrstuvwxyz";
+  test 42 "0123456789";
+  test 2 "OI2EAS67B9";
+  test 0 "Damned !";
+  test 42 "";
+  test 1 "NBzlk qnbjr !"
