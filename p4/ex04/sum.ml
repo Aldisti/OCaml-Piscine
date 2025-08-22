020a83914900806e799cb72a76e67d9545283b87
aldisti <alessandrodiste99@gmail.com>
2025-08-22 11:56:54 +0200

    d4 ex04 ready

--- p4/ex04/sum.ml
+++ p4/ex04/sum.ml
@@ -1,5 +1,5 @@
 
-let add (a: float) (b: float) : float = (+.) a b
+let add (a: float) (b: float) : float = a +. b
 
 let test a b =
   Printf.printf "%.2f + %.2f = %.2f\n" a b (add a b)
@@ -7,4 +7,5 @@ let test a b =
 let () =
   test 4.5 2.3;
   test 1.2 1.69;
-  test 3.6 0.
+  test 3.6 0.;
+  test 0. (-42.);
