a859e37819fe7899cc3c7157e80865f88dec90dd
aldisti <alessandrodiste99@gmail.com>
2025-08-22 10:30:13 +0200

    d1 ex06 ready

--- p1/ex06/iter.ml
+++ p1/ex06/iter.ml
@@ -1,8 +1,8 @@
 
 let rec iter (f: int -> int) x n =
   if n < 0 then -1
-  else if n == 0 then (x)
-  else if n == 1 then (f x)
+  else if n = 0 then x
+  else if n = 1 then f x
   else iter f (f x) (n - 1)
 
 (* TEST SUITE *)
