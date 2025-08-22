1283a5b27f3fe1061664a75d760c408ae6245479
aldisti <alessandrodiste99@gmail.com>
2025-08-22 11:54:17 +0200

    d4 ex03 ready

--- p4/ex03/jokes.ml
+++ p4/ex03/jokes.ml
@@ -9,21 +9,13 @@ let read_file filename : string array =
   with End_of_file -> close_in channel;
   !final
 
-let print arr =
-  for i = 0 to ((Array.length arr) - 1) do
-    print_endline arr.(i)
-  done
-
 let get_random arr =
-  Random.self_init ();
   arr.(Array.length arr |> Random.int)
 
-let main argv =
-  if Array.length argv != 2 then
-    print_endline "Error: Invalid number of arguments."
-  else
-    let jokes = read_file argv.(1) in
-    get_random jokes |> print_endline
+let main = function
+  | [|_; e|] -> read_file e |> get_random |> print_endline
+  | _ -> print_endline "Error: Invalid number of arguments."
 
 let () =
+  Random.self_init ();
   main Sys.argv
