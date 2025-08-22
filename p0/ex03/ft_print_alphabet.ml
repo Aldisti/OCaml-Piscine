344df53e4e4287599f4ff49b33b2c5f95e5af468
aldisti <alessandrodiste99@gmail.com>
2025-08-22 09:25:26 +0200

    d0 ex03 ready

--- p0/ex03/ft_print_alphabet.ml
+++ p0/ex03/ft_print_alphabet.ml
@@ -1,14 +1,14 @@
 
 let ft_print_alphabet unit : unit =
   let rec loop = fun s e ->
-    if s >= e then () else (
+    if s >= e then ()
+    else (
       print_char (char_of_int s);
       loop (s + 1) e
     ) in
-  loop 97 (97 + 26); print_char '\n'
+  loop 97 (97 + 26);
+  print_char '\n'
 
 (* TEST SUITE *)
 
-let main unit = ft_print_alphabet ()
-
-let () = main ()
+let () = ft_print_alphabet ()
