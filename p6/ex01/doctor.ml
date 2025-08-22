67dd4662e13c696797019926495f96b515463846
aldisti <alessandrodiste99@gmail.com>
2025-08-22 14:46:01 +0200

    d6 ex01 ready

--- p6/ex01/doctor.ml
+++ p6/ex01/doctor.ml
@@ -22,24 +22,24 @@ let tardis = "     ___________
 class doctor name age sidecick =
   object (self)
 
-  val _name : string = name
-  val mutable _age : int = age
-  val mutable _hp : int = 100
+  val name : string = name
+  val mutable age : int = age
+  val mutable hp : int = 100
   val _sidecick : People.people = sidecick
 
-  initializer Printf.printf "Creating doctor %s, age: %d, hp: %d and sidecick: %s\n" _name _age _hp (_sidecick#to_string)
+  initializer Printf.printf "Creating doctor %s, age: %d, hp: %d and sidecick: %s\n" name age hp (_sidecick#to_string)
 
   method to_string : string =
-    Printf.sprintf "{name: %s, age: %d, hp: %d, sidecick: %s}" _name _age _hp (_sidecick#to_string)
+    Printf.sprintf "Doctor(name: %s, age: %d, hp: %d, sidecick: %s)" name age hp (_sidecick#to_string)
 
   method talk : unit = print_endline "Hi! I'm the Doctor!"
 
   method travel_in_time (start: int) (arrival: int) : unit =
-    let new_age = _age + (arrival - start) in
+    let new_age = age + (arrival - start) in
     if new_age <= 0 then
       print_endline "Cannot use the tardis!"
     else (
-      _age <- new_age; print_endline tardis
+      age <- new_age; print_endline tardis
     )
 
   method use_sonic_screwdriver =
@@ -47,9 +47,9 @@ class doctor name age sidecick =
     self#regenerate
 
   method take_damage dmg =
-    if _hp < dmg then _hp <- 0
-    else _hp <- _hp - dmg
+    if hp < dmg then hp <- 0
+    else hp <- hp - dmg
 
-  method private regenerate = _hp <- 100
+  method private regenerate = hp <- 100
 
 end
