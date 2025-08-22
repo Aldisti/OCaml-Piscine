7039e49ccc5f79b3d49d43a29ec944f996313788
aldisti <alessandrodiste99@gmail.com>
2025-08-22 11:22:58 +0200

    d3 ex02 ready

--- p3/ex02/Card.ml
+++ p3/ex02/Card.ml
@@ -1,5 +1,5 @@
 module Color = struct
-  type color = Spade | Heart | Diamond | Club
+  type t = Spade | Heart | Diamond | Club
 
   let all = [Spade; Heart; Diamond; Club]
 
@@ -18,7 +18,7 @@ module Color = struct
 end
 
 module Value = struct
-  type value = T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10 | Jack | Queen | King | Ace
+  type t = T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10 | Jack | Queen | King | Ace
 
   let all = [T2; T3; T4; T5; T6; T7; T8; T9; T10; Jack; Queen; King; Ace]
 
@@ -99,7 +99,7 @@ module Value = struct
 
 end
 
-type card = (Value.value * Color.color)
+type card = (Value.t * Color.t)
 
 (* val newCard : Value.t -> Color.t -> t *)
 let newCard v c =
