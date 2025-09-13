a4c99e43c7f36704b8603bcf0306df51b38097df
aldisti <alessandrodiste99@gmail.com>
2025-09-13 09:44:10 +0200

    d7 ex04 ready

--- p7/ex04/main.ml
+++ p7/ex04/main.ml
@@ -9,6 +9,7 @@ let () =
     Printf.printf "----- %d ------\n" i;
     let alk = new Alkanes.alkane i in
     let r = new Reactions.alkane_combustion [alk] in
+    Printf.printf "%s + O2 → CO2 + H2O [%b]\n" alk#formula r#is_balanced;
     try print r with
     | Failure msg -> (
       Printf.printf "Error: %s\n" msg;
